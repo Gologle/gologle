d6e3b7beed149d977d216f985f661c8466474a9b
Enmanuel Verdesia Suarez <everdesiasuarez@gmail.com>
2022-06-08 12:53:19 -0400

    test: Add tests for case sensitivity in queries

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,29 +3,33 @@ from typing import Callable
 import pytest
 from fastapi.testclient import TestClient
 
-from src.main import app
+from src.main import app, Dataset
 
 
 client = TestClient(app)
 
 
 @pytest.mark.parametrize(
-    "q, eval_len", [
-        ("even event", lambda x: len(x) > 0),
-        ("shock wave", lambda x: len(x) > 0),
-        ("Lorem 001 ipsum", lambda x: len(x) > 0),
-        ("strength", lambda x: len(x) > 0),
-        ("the FLDSMDFR", lambda x: len(x) == 0),
-        ("Dumv qu3ry", lambda x: len(x) == 0),
-        ("ThisGetsNoResult", lambda x: len(x) == 0),
-        ("a", lambda x: len(x) == 0),
+    ["dataset", "q", "eval_len"],
+    [
+        (Dataset.cranfield, "even event", lambda x: len(x) > 0),
+        (Dataset.cranfield, "shock wave", lambda x: len(x) > 0),
+        (Dataset.cranfield, "Lorem 001 ipsum", lambda x: len(x) > 0),
+        (Dataset.cranfield, "strength", lambda x: len(x) > 0),
+        (Dataset.cranfield, "the FLDSMDFR", lambda x: len(x) == 0),
+        (Dataset.cranfield, "Dumv qu3ry", lambda x: len(x) == 0),
+        (Dataset.cranfield, "ThisGetsNoResult", lambda x: len(x) == 0),
+        (Dataset.cranfield, "a", lambda x: len(x) == 0),
+        (Dataset.newsgroups, "Rochester", lambda x: len(x) > 0),
+        (Dataset.newsgroups, "Kekule", lambda x: len(x) > 0),
+        (Dataset.newsgroups, "Lorem ipsum Kekule", lambda x: len(x) > 0),
     ]
 )
-def test_query_with_data(q: str, eval_len: Callable):
-    resp = client.get(f"/query?q={q}&model=vectorial")
+def test_query_with_data(dataset: Dataset, q: str, eval_len: Callable):
+    resp = client.get(f"/query?q={q}&model=vectorial&dataset={dataset.value}")
     assert resp.status_code == 200
     data = resp.json()
-    assert eval_len(data["results"])
+    assert eval_len(data["results"]), f"Results: {data['results']}"
 
 
 @pytest.mark.parametrize(
