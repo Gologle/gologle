fdcc37784a7c176deb52ef1540d2ce244e21e19e
Enmanuel Verdesia Suarez <everdesiasuarez@gmail.com>
2022-06-24 23:59:49 -0400

    feat: Add context manager to measure time

--- src/utils/timeit.py
+++ src/utils/timeit.py
@@ -1,4 +1,5 @@
 import time
+from contextlib import ContextDecorator
 
 
 def timed(method, *args, **kwargs):
@@ -11,3 +12,19 @@ def timed(method, *args, **kwargs):
     te = time.time()
     print("TE", te)
     return (result, (te - ts) * 1000)
+
+
+class TimeLogger(ContextDecorator):
+    start = None
+
+    def __init__(self, message):
+        super(TimeLogger, self).__init__()
+        self.message = message
+
+    def __enter__(self):
+        print(self.message, end="", flush=True)
+        self.start = time.time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        elapsed_time = round(time.time() - self.start, 2)
+        print(f"Done, took {elapsed_time} seconds.", flush=True)
